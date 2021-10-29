f7278bf54b1b66eed9b9fea5ce2f934b122f435b
Samrath Chauhan <samrathchauhan49@gmail.com>
2021-10-29 23:29:55 +0530

    JSON testing

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req,res) => {
-    res.send("Hello Universe 🌌")
+    // res.send("Hello Universe 🌌")
+    res.status(200).json({message: "Error occurred"})
 })
 
 app.listen(7000)
