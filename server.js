816e15231722240be995bf9769037ee5f12d9b0a
Samrath Chauhan <samrathchauhan49@gmail.com>
2021-10-30 15:49:37 +0530

    Ejs setup

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 
+app.set("view engine", 'ejs')
+
 app.get('/', (req,res) => {
     // res.download("./package-lock.json")
     // res.status(200).json({message: "Error occurred"})
