21b9dad2df14844504a0f4f5de37293c7f3a63ca
Samrath Chauhan <samrathchauhan49@gmail.com>
2021-10-30 22:18:18 +0530

    Added submit name

--- routes/users.js
+++ routes/users.js
@@ -1,35 +1,44 @@
 const express = require("express");
-const router = express.Router()    
+const router = express.Router();
 
-router.get("/", (req,res) => {
-    res.send("User List")
-})
+router.get("/", (req, res) => {
+  res.send("User List");
+});
 
-router.get("/new", (req,res) => {
-    res.render("users/new", {firstName: "Kumar"})
-})
+router.get("/new", (req, res) => {
+  res.render("users/new", { firstName: "Kumar" });
+});
 
-router.post('/', (req, res) => {
-    console.log(req.body.firstName)
-    res.send("Create User")
-})
+router.post("/", (req, res) => {
+  const isValid = false;
 
-router.route("/:id")
-.get((req,res) => {
-    console.log(req.user)
-    res.send(`Get user with ID ${req.params.id}`)
-})
-.put((req,res) => {
-    res.send(`Update user with ID ${req.params.id}`)
-})
-.delete((req,res) => {
-    res.send(`Delete user with ID ${req.params.id}`)
-})
+  if (isValid) {
+    users.push({ firstName: req.body.firstName });
+    res.redirect(`/users/${users.length - 1}`);
+  } else {
+    console.log("Error occurred");
+    res.render("users/new", { firstName: req.body.firstName });
+  }
+  res.send("Create User");
+});
 
-const users = [{name: "Amit"}, {name: "Sapna"}, {name: "Tarun"}]
-router.param("id", (req,res,next,id) => {
-    req.user = users[id]
-    next()
-})
+router
+  .route("/:id")
+  .get((req, res) => {
+    console.log(req.user);
+    res.send(`Get user with ID ${req.params.id}`);
+  })
+  .put((req, res) => {
+    res.send(`Update user with ID ${req.params.id}`);
+  })
+  .delete((req, res) => {
+    res.send(`Delete user with ID ${req.params.id}`);
+  });
 
-module.exports =  router
+const users = [{ name: "Amit" }, { name: "Sapna" }, { name: "Tarun" }];
+router.param("id", (req, res, next, id) => {
+  req.user = users[id];
+  next();
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,26 +1,27 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-app.use(express.static("public"))
-app.use(express.urlencoded({extended:true}))
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json())
 
-app.set("view engine", 'ejs')
+app.set("view engine", "ejs");
 // app.use(logger)
 
-app.get('/', (req,res) => {
-    // res.download("./package-lock.json")
-    // res.status(200).json({message: "Error occurred"})
-    res.render('index', {message: "Universe 🌌"})
-    // If we want to apply middleware to any route we can just pass logger as function here
-    // Can also define logger in user where we will log every user req
-})
+app.get("/", (req, res) => {
+  // res.download("./package-lock.json")
+  // res.status(200).json({message: "Error occurred"})
+  res.render("index", { message: "Universe 🌌" });
+  // If we want to apply middleware to any route we can just pass logger as function here
+  // Can also define logger in user where we will log every user req
+});
 
-const userRouter = require('./routes/users');
-app.use("/users", userRouter)
+const userRouter = require("./routes/users");
+app.use("/users", userRouter);
 
-function logger(req,res,next){
-    console.log("Req to Url: ",req.originalUrl)
-    next()
+function logger(req, res, next) {
+  console.log("Req to Url: ", req.originalUrl);
+  next();
 }
 
-app.listen(7000)
+app.listen(7000);
