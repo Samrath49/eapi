eacdd45c2d7ae276c2896ecf8b39a9254a96ebf1
Samrath Chauhan <samrathchauhan49@gmail.com>
2021-10-30 15:47:06 +0530

    EJS view engine

--- server.js
+++ server.js
@@ -2,8 +2,9 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req,res) => {
-    // res.send("Hello Universe 🌌")
-    res.status(200).json({message: "Error occurred"})
+    // res.download("./package-lock.json")
+    // res.status(200).json({message: "Error occurred"})
+    res.render('index')
 })
 
 app.listen(7000)
