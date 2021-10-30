eb83bcfc24c6f8af1631b238308e3deb347a2cc8
Samrath Chauhan <samrathchauhan49@gmail.com>
2021-10-30 19:53:42 +0530

    Middleware

--- routes/users.js
+++ routes/users.js
@@ -13,8 +13,22 @@ router.post('/', (req, res) => {
     res.send("Create User")
 })
 
-router.get("/:id", (req,res) => {
+router.route("/:id")
+.get((req,res) => {
+    console.log(req.user)
     res.send(`Get user with ID ${req.params.id}`)
+})
+.put((req,res) => {
+    res.send(`Update user with ID ${req.params.id}`)
+})
+.delete((req,res) => {
+    res.send(`Delete user with ID ${req.params.id}`)
+})
+
+const users = [{name: "Amit"}, {name: "Sapna"}, {name: "Tarun"}]
+router.param("id", (req,res,next,id) => {
+    req.user = users[id]
+    next()
 })
 
 module.exports =  router
