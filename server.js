fd89e3f14beb0ab60ffb5476f53677b539856bee
Samrath Chauhan <samrathchauhan49@gmail.com>
2021-10-30 20:00:35 +0530

    Get url using api

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 
 app.set("view engine", 'ejs')
+app.use(logger)
 
 app.get('/', (req,res) => {
     // res.download("./package-lock.json")
@@ -11,5 +12,10 @@ app.get('/', (req,res) => {
 
 const userRouter = require('./routes/users');
 app.use("/users", userRouter)
+
+function logger(req,res,next){
+    console.log(req.originalUrl)
+    next()
+}
 
 app.listen(7000)
